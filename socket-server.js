b79e22899e8e5fc9d46d273121e4d2c1ae8cdc17
Daniel Walker <polyesterhat@gmail.com>
2017-03-04 11:39:00 -0500

    add MOAR comment!

--- socket-server.js
+++ socket-server.js
@@ -9,10 +9,20 @@ const port = process.env.PORT || 3001;
 redis.subscribe('test-channel', function(err, count) {
 });
 
+// when the PHP api sends an event, 
+// push it to socket.io
 redis.on('message', function(channel, message) {
-    console.log('Message Recieved: ' + message);
-    message = JSON.parse(message);
-    io.emit(channel + ':' + message.event, message.data);
+
+  // message will look like:
+  // {"event":"App\\Events\\BeginSlides","data":{"data":{"power":"10"},"socket":null},"socket":null}
+
+  console.log('Message Recieved: ', message);
+  message = JSON.parse(message);
+
+  // event being pushed to socket will look like:
+  // R:App\\Events\\BeginSlides with the message.data as payload
+  const event = channel + ':' + message.event;
+  io.emit(event, message.data);
 });
 
 http.listen(port, function(){
