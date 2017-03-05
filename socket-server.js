343b799ce953b6dd5a974bce792f374e4ca642cf
Daniel Walker <polyesterhat@gmail.com>
2017-03-04 21:08:43 -0500

    also, give socket server the reds url, duh!

--- socket-server.js
+++ socket-server.js
@@ -1,10 +1,12 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const Redis = require('ioredis');
-const redis = new Redis();
 
 const port = process.env.PORT || 3001;
+const redisUrl = process.env.REDIS_URL || '';
+
+const Redis = require('ioredis');
+const redis = new Redis(redisUrl);
 
 var state = {
   lastPresenterEvent: {}
